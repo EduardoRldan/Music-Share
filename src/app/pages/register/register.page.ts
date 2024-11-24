0ddf6067220f524fcc577ca515743c542ef80475
EduardoRldan <163453549+EduardoRldan@users.noreply.github.com>
2024-11-24 17:24:38 -0300

    Errores de la pagina en blanco solucionado

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
-export class RegisterPage implements OnInit {
+export class RegisterPage {
+  email: string | undefined;
+  password: string | undefined;
 
   constructor() { }
 
-  ngOnInit() {
+  onRegister() {
+    // Aquí puedes manejar la lógica de registro, por ejemplo, enviar los datos a un servicio
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
   }
-
 }
