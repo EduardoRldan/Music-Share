b84261adb7e6b2e704838442214910935f25741a
EduardoRldan <163453549+EduardoRldan@users.noreply.github.com>
2024-12-06 00:12:55 -0300

    Cambios pequeños hechos en el app.modules.ts y auth.services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,11 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then(m =>m.LoginPageModule)
+  }
+  ,
+  {
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)
   },
